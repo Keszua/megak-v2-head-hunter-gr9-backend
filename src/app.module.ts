f2690efeadb253e08f26dcfb09438e853e4a9966
Karol M <keszua@gmail.com>
2023-04-18 12:58:37 +0200

    Update project

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ormConfig } from './config/ormconfig';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+        isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(ormConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/config.example.ts
@@ -0,0 +1,17 @@
+/*
+//Chang name this file to: config.ts
+//Uncomment and remove this comment
+
+export const config = {
+    //database
+	dbHost: 'localhost',
+    dbPort: 3306,
+	dbUser: 'you_name',
+	dbPassword: 'you_pass',
+	dbDatabase: 'megak_head_hunter',
+
+    //ORM config
+    ormLogging: true,       // recommended: local- true;  remote- false
+    ormSynchronize: true,   // recommended: local- true;  remote- false
+};
+*/
