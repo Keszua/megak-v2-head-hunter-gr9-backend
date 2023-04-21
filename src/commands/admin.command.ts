ecf2d42d0bdf24dac8979b7f37c83ecb60a5044a
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-04-21 17:53:01 +0200

    Add logger

--- src/commands/admin.command.ts
+++ src/commands/admin.command.ts
@@ -1,7 +1,7 @@
 import { plainToInstance } from 'class-transformer';
 import { validate } from 'class-validator';
 
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Command, Console } from 'nestjs-console';
 
 import { AdminService } from '../admin/admin.service';
@@ -12,6 +12,7 @@ import { CreateAdminDto } from '../admin/dto/create-admin.dto';
   command: 'admin',
 })
 export class AdminCommand {
+  private readonly logger = new Logger(AdminCommand.name);
   constructor(private readonly adminService: AdminService) {}
 
   @Command({
@@ -23,10 +24,10 @@ export class AdminCommand {
     const errors = await validate(createAdminDto);
 
     if (errors.length > 0) {
-      console.error('Validation failed:', errors[0].constraints);
+      this.logger.error('Validation failed:', errors[0].constraints);
       return;
     }
     await this.adminService.createAdmin(createAdminDto);
-    console.log(`Admin account created with email: ${email}`);
+    this.logger.log(`Admin account created with email: ${email}`);
   }
 }
