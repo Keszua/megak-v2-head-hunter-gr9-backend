567e17488e609f56611b616a5040454f810a470a
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-04-23 20:28:14 +0200

    Generate 'studentsGrades' service

--- src/students/students.module.ts
+++ src/students/students.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Student, StudentGrades } from './entities';
+import { StudentGradesService } from './student-grades.service';
 import { StudentsController } from './students.controller';
 import { StudentsService } from './students.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Student, StudentGrades])],
   controllers: [StudentsController],
-  providers: [StudentsService],
+  providers: [StudentsService, StudentGradesService],
 })
 export class StudentsModule {}
