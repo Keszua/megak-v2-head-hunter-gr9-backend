204ffa75a89d533831009dd3424c5f0e9f9f235d
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-04-27 00:21:35 +0200

    HH43: add LocalAuthGuard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,12 @@
-import { Body, Controller, HttpCode, Patch } from '@nestjs/common';
+import { Body, Controller, HttpCode, Patch, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { ApiBody, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Request, Response } from 'express';
 
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto';
+import { LocalAuthGuard } from './guards';
+
+import { UserResponse } from '../types';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -16,5 +20,12 @@ export class AuthController {
   @Patch('register')
   register(@Body() registerDto: RegisterDto): Promise<void> {
     return this.authService.register(registerDto);
+  }
+
+  @HttpCode(200)
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  login(@Req() req: Request, @Res({ passthrough: true }) res: Response): UserResponse {
+    return this.authService.login(req.user as UserResponse, res);
   }
 }
