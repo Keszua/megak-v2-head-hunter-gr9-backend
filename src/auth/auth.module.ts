11a7b29e1d9cd1851d928195934b0e5ea9050f91
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-04-27 00:03:20 +0200

    HH43: add CookiesService to AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,12 +3,13 @@ import { PassportModule } from '@nestjs/passport';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { CookiesService } from './cookies.service';
 
 import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [UsersModule, PassportModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, CookiesService],
 })
 export class AuthModule {}
