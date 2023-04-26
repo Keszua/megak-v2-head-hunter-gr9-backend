ded4372dd2891c7594e7598013a6c3988170eb3a
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-04-26 06:25:44 +0200

    HH55: add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
+import { swaggerConfig } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/docs', app, document);
+
   const configService = app.get(ConfigService);
   const PORT = configService.get('APP_PORT');
   await app.listen(PORT, () => {
