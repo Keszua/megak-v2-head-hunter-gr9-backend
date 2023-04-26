79765cc32601fd77995f14249b8d0a5dd48127da
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-04-26 06:31:31 +0200

    HH55: implement swagger in 'register' method

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,11 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString } from 'class-validator';
 
 import { RegisterRequest } from '../../types';
 // TODO add password validation
 export class RegisterDto implements RegisterRequest {
+  @ApiProperty({
+    description: 'Email użytkownika',
+    example: 'user@example.com',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'Hasło użytkownika',
+    example: 'password123',
+  })
   @IsString()
   password: string;
 }
