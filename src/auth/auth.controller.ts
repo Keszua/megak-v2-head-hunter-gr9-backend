c5f6705cd4939ea9ad2469497da6f3beb45149e4
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-04-26 05:53:42 +0200

    HH55: add a 'register' method and implement it

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @HttpCode(200)
+  @Post('register')
+  async register(@Body() registerDto: RegisterDto): Promise<void> {
+    return this.authService.register(registerDto);
+  }
+}
