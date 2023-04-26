7dacd4a9f9beac0f708971691dc0d690d7d5d7a2
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-04-26 06:17:00 +0200

    HH55: set global 'api' prefix

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   const configService = app.get(ConfigService);
   const PORT = configService.get('APP_PORT');
   await app.listen(PORT, () => {
