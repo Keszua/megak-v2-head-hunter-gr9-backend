ccee5692857c76043aa6a28f00b8b62aa5057a51
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-04-26 07:54:03 +0200

    HH43: install paszport packages

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -6,7 +7,7 @@ import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PassportModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
