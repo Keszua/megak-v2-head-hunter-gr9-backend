82a7d78e227b563815fe8f6d496366701a554a99
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-04-27 07:52:26 +0200

    HH43: add 'login' endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,24 @@
-import { Body, Controller, HttpCode, Patch, Post, Req, Res, UseGuards } from '@nestjs/common';
-import { ApiBody, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Patch,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiBody, ApiOkResponse, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Request, Response } from 'express';
 
 import { AuthService } from './auth.service';
-import { RegisterDto } from './dto';
+import { LoginDto, RegisterDto } from './dto';
 import { LocalAuthGuard } from './guards';
 
+import { CurrentUser } from '../common/decorators';
 import { UserResponse } from '../types';
+import { User } from '../users/entities/user.entity';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -16,16 +28,23 @@ export class AuthController {
   @ApiOperation({ summary: 'Rejestruje użytkownika i aktywuje konto' })
   @ApiBody({ type: RegisterDto })
   @ApiOkResponse({ description: 'Użytkownik zarejestrowany i konto aktywowane' })
-  @HttpCode(200)
+  @HttpCode(HttpStatus.OK)
   @Patch('register')
   register(@Body() registerDto: RegisterDto): Promise<void> {
     return this.authService.register(registerDto);
   }
 
-  @HttpCode(200)
+  @ApiOperation({ summary: 'Loguje użytkownika i zwraca tokeny w cookies' })
+  @ApiBody({ type: LoginDto })
+  @ApiOkResponse({ description: 'Użytkownik zalogowany i tokeny w cookies' })
+  @ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: 'Nieprawidłowe dane logowania' })
+  @HttpCode(HttpStatus.OK)
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  login(@Req() req: Request, @Res({ passthrough: true }) res: Response): UserResponse {
-    return this.authService.login(req.user as UserResponse, res);
+  login(
+    @CurrentUser() user: User,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<UserResponse> {
+    return this.authService.login(user, res);
   }
 }
