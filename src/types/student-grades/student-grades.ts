903fe7a5fee1a304a565e78a854275aa0d31e1ea
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-04-27 20:15:55 +0200

    HH22: add StudentGrade interface

---
+++ src/types/student-grades/student-grades.ts
@@ -0,0 +1,8 @@
+export interface StudentGrade {
+  email: string;
+  courseCompletion: number;
+  courseEngagement: number;
+  projectDegree: number;
+  teamProjectDegree: number;
+  bonusProjectUrls: string;
+}
