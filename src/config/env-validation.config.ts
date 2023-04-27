c71b84c44741a79302c27888ad61099f2d9242ff
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-04-27 05:45:20 +0200

    HH43: add environment variables

--- src/config/env-validation.config.ts
+++ src/config/env-validation.config.ts
@@ -11,5 +11,13 @@ export const envValidationObjectSchema = Joi.object({
   DB_DATABASE: Joi.string().required(),
   DB_SYNCHRONIZE: Joi.boolean().required(),
   DB_LOGGING: Joi.boolean().required(),
+  JWT_SECRET_ACTIVATION_TOKEN: Joi.string().required(),
+  JWT_EXPIRATION_TIME_ACTIVATION_TOKEN: Joi.number().required(),
+  JWT_SECRET_PASSWORD_RESET_TOKEN: Joi.string().required(),
+  JWT_EXPIRATION_TIME_PASSWORD_RESET_TOKEN: Joi.number().required(),
+  JWT_SECRET_REFRESH_TOKEN: Joi.string().required(),
+  JWT_EXPIRATION_TIME_REFRESH_TOKEN: Joi.number().required(),
+  JWT_SECRET_AUTHENTICATION_TOKEN: Joi.string().required(),
+  JWT_EXPIRATION_TIME_AUTHENTICATION_TOKEN: Joi.number().required(),
   CLIENT_URL: Joi.string().required(),
 });
--- src/tokens/tokens.service.ts
+++ src/tokens/tokens.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class TokensService {}
+export class TokensService {
+  private readonly jwtSecretActivationToken = this.configService.get<string>(
+    'JWT_SECRET_ACTIVATION_TOKEN',
+  );
+  private readonly jwtExpirationTimeActivationToken = this.configService.get<number>(
+    'JWT_EXPIRATION_TIME_ACTIVATION_TOKEN',
+  );
+  private readonly jwtSecretPasswordResetToken = this.configService.get<string>(
+    'JWT_SECRET_PASSWORD_RESET_TOKEN',
+  );
+  private readonly jwtExpirationTimePasswordResetToken = this.configService.get<number>(
+    'JWT_EXPIRATION_TIME_PASSWORD_RESET_TOKEN',
+  );
+  private readonly jwtSecretRefreshToken = this.configService.get<string>(
+    'JWT_SECRET_REFRESH_TOKEN',
+  );
+  private readonly jwtExpirationTimeRefreshToken = this.configService.get<number>(
+    'JWT_EXPIRATION_TIME_REFRESH_TOKEN',
+  );
+  private readonly jwtSecretAuthenticationToken = this.configService.get<string>(
+    'JWT_SECRET_AUTHENTICATION_TOKEN',
+  );
+  private readonly jwtExpirationTimeAuthenticationToken = this.configService.get<number>(
+    'JWT_EXPIRATION_TIME_AUTHENTICATION_TOKEN',
+  );
+  private readonly clientUrl = this.configService.get<string>('CLIENT_URL');
+
+  constructor(private readonly configService: ConfigService) {}
+}
