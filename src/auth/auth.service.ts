74c0c29fac08bde4a66418649686b82e7de0a9b2
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-04-27 07:45:27 +0200

    HH43: add authorization token generation implementations

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
 import { Response } from 'express';
 
+import { CookiesService } from './cookies.service';
 import { LoginDto, RegisterDto } from './dto';
 
-import { UserResponse } from '../types';
+import { TokensService } from '../tokens/tokens.service';
+import { CookiesNames, TokenOptions, UserResponse } from '../types';
+import { User } from '../users/entities/user.entity';
 import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly tokensService: TokensService,
+    private readonly cookiesService: CookiesService,
+  ) {}
   async register(registerDto: RegisterDto): Promise<void> {
     const user = await this.usersService.getUserByEmail(registerDto.email);
     await this.usersService.changePassword(user.id, registerDto.password);
@@ -32,7 +39,40 @@ export class AuthService {
     return null;
   }
 
-  login(user: UserResponse, res: Response): UserResponse {
+  async login(user: User, res: Response): Promise<UserResponse> {
+    await this.renewAuthAndRefreshTokensAndSetCookies(user, res);
     return user;
+  }
+
+  async generateAuthTokenAndSetCookie(
+    user: User,
+    res: Response,
+    options: TokenOptions,
+  ): Promise<void> {
+    let cookieName: CookiesNames;
+    const { tokenType } = options;
+    const { token, expiresIn } = await this.tokensService.createToken(user, { tokenType });
+
+    switch (tokenType) {
+      case 'refresh':
+        cookieName = CookiesNames.REFRESH;
+        break;
+      case 'authentication':
+        cookieName = CookiesNames.AUTHENTICATION;
+        break;
+      default:
+        throw new BadRequestException('Invalid token type');
+    }
+
+    this.cookiesService.setTokenInCookie(res, cookieName, {
+      token,
+      expiresIn,
+    });
+  }
+
+  async renewAuthAndRefreshTokensAndSetCookies(user: User, res: Response): Promise<void> {
+    // TODO: add revoke active refresh token
+    await this.generateAuthTokenAndSetCookie(user, res, { tokenType: 'authentication' });
+    await this.generateAuthTokenAndSetCookie(user, res, { tokenType: 'refresh' });
   }
 }
