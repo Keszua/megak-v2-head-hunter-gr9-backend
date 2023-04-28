07e7be4950289f341435d5ed9fbc02fff8b82592
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-04-28 18:46:00 +0200

    HH65: add cors config

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { corsOptions } from './config';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  app.enableCors(corsOptions);
   const PORT = configService.get('APP_PORT');
   await app.listen(PORT, () => {
     Logger.log(`Server is running on port ${PORT}`);
