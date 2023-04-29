01469b1c100c3221ff0079e3b22e0c1968fa172e
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-04-29 22:33:41 +0200

    HH43: add JwtStrategy and JwtAuthGuard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { CookiesService } from './cookies.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 
 import { TokensModule } from '../tokens/tokens.module';
@@ -12,6 +13,6 @@ import { UsersModule } from '../users/users.module';
 @Module({
   imports: [UsersModule, PassportModule, TokensModule],
   controllers: [AuthController],
-  providers: [AuthService, CookiesService, LocalStrategy],
+  providers: [AuthService, CookiesService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
