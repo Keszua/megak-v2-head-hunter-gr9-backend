b0098a972506291f220a631db2b8db07844c62b2
EckhartPL <mateusz.masek@wp.pl>
2023-04-29 10:24:17 +0200

    HH51 Mailer module - saving current work to switch branches

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { MailerModule } from '@nestjs-modules/mailer';
 import { ConsoleModule } from 'nestjs-console';
 
 import { AdminModule } from './admin/admin.module';
@@ -9,12 +10,18 @@ import { AppService } from './app.service';
 import { AdminCommand } from './commands/admin.command';
 import { GlobalExceptionFilter, GlobalResponseInterceptor } from './common';
 import { envValidationObjectSchema } from './config';
+import { mailerConfig } from './config/mailer.config';
 import { DatabaseModule } from './database/database.module';
 import { StudentsModule } from './students/students.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    MailerModule.forRootAsync({
+      useFactory: () => ({
+        ...mailerConfig,
+      }),
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: envValidationObjectSchema,
