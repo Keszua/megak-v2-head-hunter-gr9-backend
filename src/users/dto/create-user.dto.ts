30d78f245e7454461028cdfe09243d094012bc39
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-05-02 11:28:11 +0200

    HH68: update dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,14 +1,10 @@
-import { IsEmail, IsEnum, IsOptional, IsString } from 'class-validator';
+import { IsEmail, IsEnum } from 'class-validator';
 
 import { UserRole } from '../../types';
 
 export class CreateUserDto {
   @IsEmail()
   email: string;
-
-  @IsOptional()
-  @IsString()
-  password?: string;
 
   @IsEnum(UserRole)
   role: UserRole;
