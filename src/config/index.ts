568ea7a9b50c7a55eaa12001f5493210b8ed7fdf
EckhartPL <mateusz.masek@wp.pl>
2023-05-02 02:57:36 +0200

    HH51 Mailer module - added register link pt2.

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,7 @@ export * from './typeorm.config';
 export * from './env-validation.config';
 export * from './cors.config';
 export * from './swagger.config';
+export * from './mailer.config';
 export * from './cookies.config';
 export * from './validation-pipe.config';
 export * from './mailer.config';
