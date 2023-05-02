e9fdda121736a73270d68d3c1d043fa9eb3eeab8
EckhartPL <mateusz.masek@wp.pl>
2023-05-02 03:30:56 +0200

    HH51 Mailer module - added restore password waypoint and method.

---
+++ src/utils/mail/mail-details.ts
@@ -0,0 +1,9 @@
+export enum MailSubject {
+  REGISTER = 'Link do rejestracji',
+  RESTORE_PWD = 'Odzyskiwanie hasła',
+}
+
+export enum MailTemplate {
+  REGISTER = 'register',
+  RESTORE_PWD = 'restore-pwd',
+}
