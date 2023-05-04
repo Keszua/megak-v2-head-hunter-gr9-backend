037c1dfd75a9a1c5fce0903c69a72b7e55e37751
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-05-04 20:19:26 +0200

    HH51: rebase onto develop

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,6 @@
 import { Module, ValidationPipe } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
-import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { ConsoleModule } from 'nestjs-console';
@@ -13,9 +10,14 @@ import { AuthModule } from './auth/auth.module';
 import { JwtAuthGuard } from './auth/guards';
 import { AdminCommand } from './commands/admin.command';
 import { GlobalExceptionFilter, GlobalResponseInterceptor } from './common';
-import { envValidationObjectSchema,validationPipeOptions, eventEmmiterConfig, getMailerConfig } from './config';
+import {
+  envValidationObjectSchema,
+  validationPipeOptions,
+  eventEmmiterConfig,
+  getMailerConfig,
+} from './config';
 import { DatabaseModule } from './database/database.module';
-import { EmailModule } from './email/email.module';
+import { EmailModule } from './email';
 import { HrModule } from './hr/hr.module';
 import { EventsModule } from './orders/events.module';
 import { StudentsModule } from './students/students.module';
